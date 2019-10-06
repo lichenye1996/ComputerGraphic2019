2516f9313ccb6258ffa0b690b96f9c2b663c860f
陈子鑫 <chenzixin@dhcp-clara-2111.redrover.cornell.edu>
2019-10-06 01:38:13 -0400

    ti

--- ray/src/ray1/camera/OrthographicCamera.java
+++ ray/src/ray1/camera/OrthographicCamera.java
@@ -1,23 +1,37 @@
 package ray1.camera;
 
+import egl.math.Vector3;
 import ray1.Ray;
 import egl.math.Vector3d;
 
 public class OrthographicCamera extends Camera {
 
     //TODO#Ray Task 1: create necessary new variables/objects here, including an orthonormal basis
-    //          formed by three basis vectors and any other helper variables 
+    //          formed by three basis vectors and any other helper variables
     //          if needed.
-	
-    
-    
+    Vector3d u;
+    Vector3d v;
+    Vector3d w;
+    Vector3d e;
+    Vector3d d;
+
+
+
     /**
      * Initialize the derived view variables to prepare for using the camera.
      */
     public void init() {
         // TODO#Ray Task 1:  Fill in this function.
-        // 1) Set the 3 basis vectors in the orthonormal basis, 
+        // 1) Set the 3 basis vectors in the orthonormal basis,
         //    based on viewDir and viewUp
+        Vector3d viewDir = new Vector3d((double)getViewDir().x, (double)getViewDir().y, (double)getViewDir().z);
+        Vector3d viewPoint = new Vector3d((double)getViewPoint().x, (double)getViewPoint().y, (double)getViewPoint().z);
+        Vector3d viewUp = new Vector3d((double)getViewUp().x, (double)getViewUp().y, (double)getViewUp().z);
+        d = (new Vector3d(viewDir.x, viewDir.y, viewDir.z)).normalize();
+        w = d.negate().normalize().clone();
+        u = viewUp.cross(w).normalize().clone();
+        v = w.cross(u).normalize().clone();
+        e = viewPoint.clone();
         // 2) Set up the helper variables if needed
 
 
@@ -32,16 +46,20 @@ public class OrthographicCamera extends Camera {
      */
     public void getRay(Ray outRay, float inU, float inV) {
         // TODO#Ray Task 1: Fill in this function.
-        // 1) Transform inU so that it lies between [-viewWidth / 2, +viewWidth / 2] 
+        // 1) Transform inU so that it lies between [-viewWidth / 2, +viewWidth / 2]
         //    instead of [0, 1]. Similarly, transform inV so that its range is
         //    [-vieHeight / 2, +viewHeight / 2]
-        // 2) Set the origin field of outRay for an orthographic camera. 
+        double width = getViewWidth();
+        double height = getViewHeight();
+        double transInU = ((double)inU/1.) * width - width / 2.;
+        double transInV = ((double)inV/1.) * height - height / 2.;
+        // 2) Set the origin field of outRay for an orthographic camera.
         //    In an orthographic camera, the origin should depend on your transformed
         //    inU and inV and your basis vectors u and v.
+        Vector3d origin = e.add(u.mul(transInU)).add(v.mul(transInV)).clone();
+        Vector3d rayDir = d.negate().clone();
         // 3) Set the direction field of outRay for an orthographic camera.
-
-        
-
+        outRay.set(origin, rayDir);
     }
 
 }
